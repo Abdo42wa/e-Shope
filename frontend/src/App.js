7890228caaa9c870ea5c16107fe5b39b09213107
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-10 20:48:10 +0300

    set up

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>home</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
