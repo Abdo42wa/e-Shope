0e6fef88739412b5209619c0f12a45a65761b607
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-17 16:03:44 +0300

    adding product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './Screens/Home';
+import Product from './Screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Home />
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={Home} exact />
+            <Route path='/Product/:id' component={Product} />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
