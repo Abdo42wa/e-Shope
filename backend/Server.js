60ca176bd584e9bb3cc4063b3cd68f1e1a3d1660
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-28 12:09:56 +0300

    generate user token with js

--- backend/Server.js
+++ backend/Server.js
@@ -2,18 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRouter from './routes/productRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 dotenv.config()
 
 
 connectDB()
 const app = express()
+//to exept json data in the body
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Api is running')
 })
 
 app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
 
 const port = process.env.PORT || 5000
 app.listen(5000, console.log(`server runing in ${process.env.NODE_ENV} mode on port ${port} `))
