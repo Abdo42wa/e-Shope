95666d5a49921d9cd993d440b824ddc00b25bc8c
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-28 23:06:02 +0300

    create a login and sign-in backend functions and routers

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,10 +16,6 @@ const userSchema = mongoose.Schema({
         type: String,
         require: true
     },
-    dateOfBirth: {
-        type: Date,
-        require: true
-    },
     isAdmin: {
         type: Boolean,
         require: true,
@@ -32,6 +28,16 @@ const userSchema = mongoose.Schema({
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
 
 
 const User = mongoose.model('User', userSchema)
