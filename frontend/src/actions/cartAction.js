6894e72fa9441117e25ac7594c922669a4b19473
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-02 20:20:10 +0200

    Shipping action and reducer

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -27,3 +27,21 @@ export const removeFromCart = (id) => (dispatch, getState) => {
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
+export const saveShippingAddress = (data) => (dispatch, getState) => {
+    dispatch({
+        type: 'CART_SAVE_SHIPING_ADDRESS',
+        payload: data
+    })
+
+    localStorage.setItem('shippingAddress', JSON.stringify(data))
+
+}
+export const savePaymentMethod = (data) => (dispatch, getState) => {
+    dispatch({
+        type: 'CART_SAVE_PAYMENT_METHOD',
+        payload: data
+    })
+
+    localStorage.setItem('paymentMethod', JSON.stringify(data))
+
+}
