36d42b8e1d430e242f04d140ca4515027d172f44
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-04 01:10:04 +0200

    delete Product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,5 +20,16 @@ const getProductById = asyncHandler(async (req, res) => {
         throw new Error('product not found')
     }
 })
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-export { getProductById, getProducts }
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export { getProductById, getProducts, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import asyncHandler from 'express-async-handler'
 const router = express.Router();
-import { getProducts, getProductById } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 // get all product
 router.route('/').get(getProducts)
 
 
 // get product by id
-router.route('/:id').get(getProductById)
+router.route('/:id')
+    .get(getProductById)
+    .delete(protect, admin, deleteProduct);
 
 export default router;
