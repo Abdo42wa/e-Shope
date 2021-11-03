9af174896723361aa315ae985e01d31d4dcc8e0e
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-04 00:08:48 +0200

    delete user endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, getUsers, registerUser, updateUserProofile } from '../controllers/userController.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProofile, deleteUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
+
+router.route('/:id').delete(protect, admin, deleteUser)
 
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProofile);
 
