6c6cd1a1a0da654a0bcddada2dc88cb84ffcbaee
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-04 02:01:32 +0200

    product list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './Screens/PlaceOrderScreen';
 import OrderScreen from './Screens/OrderScreen';
 import UserListScreen from './Screens/UserListScreen';
 import UserEditScreen from './Screens/UserEditScreen';
+import ProductListScreen from './Screens/ProductListScreen';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
             <Route path='/placeorder' component={PlaceOrderScreen} />
             <Route path='/order/:id' component={OrderScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
+            <Route path='/admin/productlist' component={ProductListScreen} />
             <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           </Container>
         </main>
