2a01fc09d89141f4b32a0f23bca7c9ff0d7f3d8b
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-04 02:23:43 +0200

    upload image endpoint

--- backend/Server.js
+++ backend/Server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import OrderRoute from './routes/orderRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import UploadRoutes from './routes/uploadRoutes.js'
+
 
 dotenv.config()
 
@@ -21,6 +23,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', OrderRoute)
+app.use('/api/upload', UploadRoutes)
 
 app.use('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
